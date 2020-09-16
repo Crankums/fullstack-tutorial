3a969507f16c8b24c0418565beee6e1b5b141994
Craig_Donavin <crdonavin@gmail.com>
2020-09-16 08:58:41 -0500

    initial committ

---
+++ start/server/src/schema.js
@@ -0,0 +1,29 @@
+const { gql } = require('apollo-server')
+
+const typeDefs = gql`
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+  }
+  
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
+  
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
+`;
+
+module.exports = typeDefs
